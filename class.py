909c37ca2e617e21c41e4e889de779e1cfd64d20
Ayush Raj (ayusraj) <ayusraj@cisco.com>
2022-02-06 03:38:56 +0530

    OOPS in Python

---
+++ class.py
@@ -0,0 +1,10 @@
+class Cisco:
+    def __init__(self) -> None:
+        print("New object of Cisco Created")
+
+    def name(self, name):
+        print(f"Name of the user is {name}")
+
+
+ayush = Cisco()
+ayush.name("Ayush Raj")
