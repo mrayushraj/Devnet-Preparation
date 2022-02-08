bd3880a3568b692d046fa5643779f40619740fad
Sigma Coder #01 <58598484+sigmacoder1@users.noreply.github.com>
2022-02-08 16:25:32 +0530

    Update scriptRequestAPI.py

--- APIs/scriptRequestAPI.py
+++ APIs/scriptRequestAPI.py
@@ -1,17 +1,17 @@
-import requests
+"""import requests
 import json
 
-class2022 = "Y2lzY29zcGFyazovL3VzL1JPT00vN2Q1MWU5ODAtNzM3OS0xMWVjLWJiNWItMTU4YzlkMDU0YmI5"
+class2022 = "Y2lzY29zcGFyazovL3OSCMSCMVzL1JPT00vN2QadmcaacalkAKNASmkvdONOInoOIG1MWU5ODAtNzM3OS0xMWVjLWJiNWItMTU4YzlkMDU0YmI5"
 
 Headers = {
-    "Authorization" : "Bearer ZWJjMzJhZTQtMTNjZS00ZTgxLWJmZjWViMjcyOTk1MjQxZWQ3NzYtMjJh_PF84_1eb6f43-417f-9974-ad72cae0e10f", 
+    "Authorization" : "Bearer KDBKBFWZWJjMzJhZTQtMTNjZS00ZTgxLWJmZjWViMjcyOTk1MjQxZWQ3NzYtMjJh_PF84_1eb6f43-417f-9974-ad72cae0e10f", 
     "Content-Type" : "application/json"
     }
 
 response = requests.post("https://webexapis.com/v1/messages",headers=Headers, 
         json={ 
-            "roomId" : class2022, 
+            "roomId" : cbsaaoico, 
             "text" : "This is a test message from API using Python script"
             })
 
-print(response.json())
+print(response.json())"""
